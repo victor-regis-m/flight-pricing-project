9d9a2acf294de77abacfb849e7e824f2cdd2ad36
Victor <victor.regism@gmail.com>
2023-03-15 20:14:58 -0300

    changing db_config reference to absolute

--- airflow_dags/scraper_orchestrator.py
+++ airflow_dags/scraper_orchestrator.py
@@ -12,11 +12,10 @@ scraper_dag = DAG(
 origin_airport = "GRU"
 destination_airport = "BER"
 departure_date = "2023-09-15"
-command = f"python3 main.py {origin_airport} {destination_airport} {departure_date}"
+command = \
+    f"python3 ~/flight-pricing-project/scraper_files/main.py {origin_airport} {destination_airport} {departure_date}"
 
 venv_activation = "source ~/env/bin/activate"
-
-files_directory = "cd ~/flight-pricing-project/scraper_files/"
 
 venv = BashOperator(
     task_id='activate_venv',
@@ -24,19 +23,11 @@ venv = BashOperator(
     dag=scraper_dag
 )
 
-change_dir = BashOperator(
-    task_id='change_dir',
-    bash_command=files_directory,
-    dag=scraper_dag
-)
-
-
 scraper = BashOperator(
     task_id='run_scraper',
     bash_command=command,
     dag=scraper_dag
 )
 
-venv.set_downstream(change_dir)
-change_dir.set_downstream(scraper)
+venv.set_downstream(scraper)
 
